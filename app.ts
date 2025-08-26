424b2f2a5990f1c45160b15238cb620045158987
Pablo Henrique <160090075+oapache@users.noreply.github.com>
2025-08-26 15:05:06 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { BonusAccount } from "./class/BonusAccount.js"
+import { CompanyAccount } from "./class/CompanyAccount.js"
+import { PeopleAccount } from "./class/PeopleAccount.js"
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
